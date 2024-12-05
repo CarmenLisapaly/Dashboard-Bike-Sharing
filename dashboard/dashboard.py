39c662283569c9c523157e566af0bee4fc2e06b6
Carmen Emanuela Dwiva Lisapaly <166214096+CarmenLisapaly@users.noreply.github.com>
2024-12-05 15:40:35 +0800

    dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Membaca data dari all_data.csv
-all_data = pd.read_csv('all_data.csv')
+all_data = pd.read_csv('dashboard/all_data.csv')
 
 # Mengatur tema dan estetika Streamlit
 st.set_page_config(page_title="Bike Sharing Dashboard", page_icon="🚲", layout="wide")
